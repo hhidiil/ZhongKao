ad5ccaac096a38dbab0ca4cd60bb4a6848a872b3
gao351306708 <351306708@qq.com>
2018-08-22 15:39:31 +0800

    20180822-3

--- src/container/front/page_modules/exciseTips/index.js
+++ src/container/front/page_modules/exciseTips/index.js
@@ -33,7 +33,7 @@ class Index extends Component{
             <div className="exciseTips">
                 <ul>
                     <li><a href="javascript:void(0);">1、中考解题技巧</a>
-                        <a href="http://localhost:10000/api/download?name=2018年广东省中考数学考试大纲.pdf" className="btnDownload">2018年广东省中考数学考试大纲(下载)</a>
+                        <a href="/api/download?name=2018年广东省中考数学考试大纲.pdf" className="btnDownload">2018年广东省中考数学考试大纲(下载)</a>
                         <a href="/api/download?name=广东省中考数学试卷结构和命题规律(2016年~2018年).pdf" className="btnDownload">广东省中考数学试卷结构和命题规律(下载)</a>
                         {/*<a href="public/docs/readme.txt" className="btnDownload">广东省中考数学试卷结构和命题规律(下载)</a>*/}
                     </li>
